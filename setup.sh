96404d72fa041bf0ef50edc8f98a13b197fcf9fe
Zhu Li <zhuli19901106@gmail.com>
2018-07-29 21:51:14 +0800

    add -f mode for setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/bash
 
+ln_opts='-s'
+for arg in $@; do
+    if [[ $arg == '-f' ]]; then
+        ln_opts='-sf'
+    fi
+done
+
 cur_dir=`readlink -f $0`
 cur_dir=`dirname $cur_dir`
 cd $cur_dir
-ln -s `readlink -f bin` -t ~/
+ln $ln_opts `readlink -f bin` -t ~/
 cd dot-conf
 for f in `ls`; do
-    ln -s `readlink -f $f` ~/.${f}
+    ln $ln_opts `readlink -f $f` ~/.${f}
 done
 cd ..
